8115220d21a3b3fde20ade53ac2a3b016243153b
simonleelcf <simonleelcf@gmail.com>
2020-06-21 21:31:02 +1000

    adding profile route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomeView from "../homepage/HomeView";
 import ServiceView from "../service/ServiceView";
+import profile from "../profile/profile";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/home" component={HomeView} />
       <Route exact path="/service" component={ServiceView} />
+      <Route exact path="/profile" component={profile} />
     </Switch>
   );
 };
